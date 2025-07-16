2c25095e91944aa52fca2fe8041225a781a0aa34
Hana-Bassem <hana.bassem@shezlong.com>
2025-07-16 23:32:58 +0300

    userSignUp

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,35 @@
-import { Controller, Get, Param } from '@nestjs/common';
-
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserService, User } from './user.service';
 @Controller('user')
 export class UserController {
-
+//create an instance of class userservice in user.service
+constructor(private userService: UserService) {}
 @Get()
 getUser():string{
     return"hello user"
 }
-//take dynmaic part of url
+//@Param('id') --> take dynmaic part of url
 @Get(":id")
 getuserid(@Param('id') id:string):string{
 return `user ${id}`;
 
+}
+@Post('signup')
+  signup(@Body() userData: User) {
+    // your signup logic here
+    return { message: 'User signed up', userData };
+  }
+@Post('signup1')
+create(@Body() userdata:User){
+    //UserService userservice=new UserService();
+   const userexist=this.userService.finduser(userdata.username);
+   if (userexist){
+    return "user are already signup";
+   }
+   else {
+    const user = this.userService.createuser(userdata);
+    return { message: 'User signed up', user };
+  }
+   
 }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+//Injectable-->something like public
+//structure
+export interface User {
+  id: number;
+  username: string;
+  password: string; 
+}
+
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+   private users: User[]=[];
+
+  createuser(user: User) {
+    this.users.push(user);
+    return user;
+  }
+  finduser(username:string){
+    //user: is a parameter to search every user in users
+    return this.users.find(user=>(user.username)=== username);
+  }
+}
